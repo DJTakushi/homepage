56caa80f52c224a50efba4fece296177a4c7808b
DJTakushi <okinawanspud@gmail.com>
2023-07-16 22:03:26 -0500

    default time for simplicity

--- homepage/wwwroot/js/site.js
+++ homepage/wwwroot/js/site.js
@@ -6,15 +6,14 @@ function applyTimes() {
     // based on https://www.plus2net.com/javascript_tutorial/clock.php
   const timeClasses = document.getElementsByClassName("time");
   for (const time_e of timeClasses){
-    const myTz = time_e.getAttribute("tz")
-    if (myTz != null){
-      // var d = new Date((new Date().getTime())+1000*myTz)
-      var d = new Date(Date.now());
-      // old style using timezone names.  OpenWeahter only supports seconds.
-      // var d_text = d.toLocaleString("en-US", {hour12: false, hour: '2-digit', minute:'2-digit'});
-      var d_text = d.toISOString().slice(11,19);//time in ISO format
-      time_e.innerHTML = d_text
+    var myTz = time_e.getAttribute("tz")
+    if (myTz == null){
+      myTz = Intl.DateTimeFormat().resolvedOptions().timeZone;
     }
+    var d = new Date(Date.now());
+    var d_local = d.toLocaleString('en-us', {timeZone: myTz, hour12:false});
+    var d_text=d_local.slice(11,19);
+    time_e.innerHTML = d_text
   }
   var refresh=1000;
   mytime = setTimeout(applyTimes,refresh);
