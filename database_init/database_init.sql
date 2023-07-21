05e3ace07c9a0c2c0178ffa4a935a3ba4413b9f4
DJTakushi <okinawanspud@gmail.com>
2023-07-21 18:42:44 -0500

    init table

--- database_init/database_init.sql
+++ database_init/database_init.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE cities;
+CREATE DATABASE IF NOT EXISTS homepage;
+CREATE TABLE IF NOT EXISTS cities (
+  cityName varchar(255) NOT NULL,
+  condition varchar(512),
+  tempC float(10,10),
+  humidity float(10,10),
+  PRIMARY KEY (cityName)
+);
